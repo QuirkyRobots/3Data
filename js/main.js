60ee325fa7bcfb5d71f4a32b963d136c0aa16362
Steveo <clickbox100@gmail.com>
2023-12-24 23:20:23 +1000

    JS order fix

--- js/main.js
+++ js/main.js
@@ -1,39 +1,46 @@
-document.addEventListener('DOMContentLoaded', function () {
-    var textInput = document.getElementById('textInput');
-    var params = new URLSearchParams(window.location.search);
-    var msg = params.get('msg');
+document.addEventListener('DOMContentLoaded', () => {
+    const params = new URLSearchParams(window.location.search);
+    const inputMappings = {
+        'msg': 'textInput',
+        'bg': 'backgroundColourPicker',
+        'cube': 'cubeColourPicker',
+        'txt': 'textColourPicker'
+    };
 
-    if (msg) {
-      textInput.value = decodeURIComponent(msg);
-    }
-  });
+    Object.keys(inputMappings).forEach(key => {
+        const value = params.get(key);
+        if (value) {
+            document.getElementById(inputMappings[key]).value = decodeURIComponent(value);
+        }
+    });
+});
 
-  document
+document
     .getElementById("backgroundColourPicker")
     .addEventListener("input", function () {
-      var colour = this.value;
-      document.getElementById("selectedBackgroundColour").innerText =
-        colour;
-      document.body.style.backgroundImage =
-        "radial-gradient(" + colour + ", transparent)";
+        var colour = this.value;
+        document.getElementById("selectedBackgroundColour").innerText =
+            colour;
+        document.body.style.backgroundImage =
+            "radial-gradient(" + colour + ", transparent)";
     });
 
-  // Cube colour picker
+// Cube colour picker
 
-  document
+document
     .getElementById("cubeColourPicker")
     .addEventListener("input", function () {
-      var colour = this.value;
-      document.getElementById("selectedCubeColour").innerText =
-        colour;
+        var colour = this.value;
+        document.getElementById("selectedCubeColour").innerText =
+            colour;
     });
 
-  // Text colour picker
+// Text colour picker
 
-  document
+document
     .getElementById("textColourPicker")
     .addEventListener("input", function () {
-      var colour = this.value;
-      document.getElementById("selectedTextColour").innerText =
-        colour;
+        var colour = this.value;
+        document.getElementById("selectedTextColour").innerText =
+            colour;
     });
