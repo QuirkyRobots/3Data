ffb72ef1fae3f479c7a4a63280b9f507846b7e0f
Steveo <clickbox100@gmail.com>
2023-12-24 14:29:46 +1000

    Fix

--- js/live-3d.js
+++ js/live-3d.js
@@ -1,7 +1,5 @@
 import * as THREE from "https://cdn.jsdelivr.net/npm/three@0.160.0/build/three.module.js";
-document.addEventListener('DOMContentLoaded', function() {
-init();
-});
+
 // Configuration variables
 
 // Spin things
