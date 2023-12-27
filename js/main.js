468cb7027c67f5be467201bfc25f085440ad059d
Steveo <clickbox100@gmail.com>
2023-12-28 00:15:52 +1000

    UI improvments, bug fixes

--- js/main.js
+++ js/main.js
@@ -1,103 +1,76 @@
-
-
-
 document.addEventListener("DOMContentLoaded", function () {
+  // Fade the page in and be super cool.
 
-    setTimeout(function() {
-        var bodyElement = document.body;
-       bodyElement.style.transition = 'opacity 1s';
-       bodyElement.style.opacity = 1;
-    }, 1000);
-    
+  setTimeout(function () {
+    var bodyElement = document.body;
+    bodyElement.style.transition = "opacity 1s";
+    bodyElement.style.opacity = 1;
+  }, 1000);
 
-  applyStylesFromURL();
+  // and and remove the weed, I mean hash, because the URL doesn't like it
 
-  const params = new URLSearchParams(window.location.search);
-  const inputMappings = {
-    msg: "textInput",
-    bg: "backgroundColourPicker",
-    cube: "cubeColourPicker",
-    txt: "textColourPicker",
+  const updateURLParam = (param, value) => {
+    const url = new URL(window.location);
+    url.searchParams.set(param, value.replace("#", ""));
+    window.history.pushState({}, "", url);
   };
 
-  Object.keys(inputMappings).forEach((key) => {
-    let value = params.get(key);
-    if (value) {
-      if (key !== "msg" && !value.startsWith("#")) {
-        value = "#" + value;
-      }
-      document.getElementById(inputMappings[key]).value =
-        decodeURIComponent(value);
+  // Read the URL and asign the values. Super fancy
+
+  const updatePageFromURL = () => {
+    const urlParams = new URLSearchParams(window.location.search);
+
+    const bgColour = urlParams.get("bg");
+    if (bgColour) {
+      const bgPicker = document.getElementById("backgroundColourPicker");
+      bgPicker.value = "#" + bgColour;
+      document.getElementById("selectedBackgroundColour").innerText =
+        "#" + bgColour;
+      document.body.style.backgroundImage = `radial-gradient(#${bgColour}, transparent)`;
+    }
+
+    const blockColour = urlParams.get("block");
+    if (blockColour) {
+      const blockPicker = document.getElementById("cubeColourPicker");
+      blockPicker.value = "#" + blockColour;
+      document.getElementById("selectedCubeColour").innerText =
+        "#" + blockColour;
+    }
+
+    const txtColour = urlParams.get("txt");
+    if (txtColour) {
+      const textPicker = document.getElementById("textColourPicker");
+      textPicker.value = "#" + txtColour;
+      document.getElementById("selectedTextColour").innerText = "#" + txtColour;
+    }
+
+    const message = urlParams.get("msg");
+    if (message) {
+      const textInput = document.getElementById("textInput");
+      textInput.value = message;
+    }
+  };
+
+  // Plonk the values in the URL
+
+  document.addEventListener("input", (event) => {
+    const elementId = event.target.id;
+    const value = event.target.value;
+
+    if (elementId === "backgroundColourPicker") {
+      document.getElementById("selectedBackgroundColour").innerText = value;
+      document.body.style.backgroundImage = `radial-gradient(${value}, transparent)`;
+      updateURLParam("bg", value);
+    } else if (elementId === "cubeColourPicker") {
+      document.getElementById("selectedCubeColour").innerText = value;
+      updateURLParam("block", value);
+    } else if (elementId === "textColourPicker") {
+      document.getElementById("selectedTextColour").innerText = value;
+      updateURLParam("txt", value);
+    } else if (elementId === "textInput") {
+      updateURLParam("msg", value);
     }
   });
+
+  updatePageFromURL();
 });
-
-// URL magic
-
-const applyStylesFromURL = () => {
-  const params = new URLSearchParams(window.location.search);
-  const bgValue = params.get("bg");
-  if (bgValue) {
-    document.body.style.backgroundImage = `radial-gradient(#${bgValue}, transparent)`;
-  }
-};
-
-const updateURLAndStyles = () => {
-  const params = new URLSearchParams();
-  const elements = {
-    msg: "textInput",
-    bg: "backgroundColourPicker",
-    cube: "cubeColourPicker",
-    txt: "textColourPicker",
-  };
-
-  for (const [key, id] of Object.entries(elements)) {
-    const element = document.getElementById(id);
-    const value =
-      key !== "msg" ? element.value.replace("#", "") : element.value;
-    params.set(key, value);
-
-    if (key === "bg") {
-      document.body.style.backgroundImage = `radial-gradient(${element.value}, transparent)`;
-    }
-  }
-  window.history.pushState({}, "", `${window.location.pathname}?${params}`);
-};
-
-[
-  "textInput",
-  "backgroundColourPicker",
-  "cubeColourPicker",
-  "textColourPicker",
-].forEach((id) =>
-  document.getElementById(id).addEventListener("input", updateURLAndStyles)
-);
-
-// Background colour picker
-
-document
-  .getElementById("backgroundColourPicker")
-  .addEventListener("input", function () {
-    var colour = this.value;
-    document.getElementById("selectedBackgroundColour").innerText = colour;
-    document.body.style.backgroundImage =
-      "radial-gradient(" + colour + ", transparent)";
-  });
-
-// Cube colour picker
-
-document
-  .getElementById("cubeColourPicker")
-  .addEventListener("input", function () {
-    var colour = this.value;
-    document.getElementById("selectedCubeColour").innerText = colour;
-  });
-
-// Text colour picker
-
-document
-  .getElementById("textColourPicker")
-  .addEventListener("input", function () {
-    var colour = this.value;
-    document.getElementById("selectedTextColour").innerText = colour;
-  });
