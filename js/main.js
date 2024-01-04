2ede71294e48c406864e67bc4c2e8ea953bc2053
Steveo <clickbox100@gmail.com>
2024-01-04 17:45:24 +1000

    v1.4.3 Controld on/off

--- js/main.js
+++ js/main.js
@@ -141,4 +141,10 @@ document.addEventListener("DOMContentLoaded", function () {
 
   // Idea: Prevent the wireframe vertices from being too big, because it will roast some devices.
 
+  // Controls on and off
+
+  document.getElementById("themeSwitch").addEventListener("change", function () {
+    var themeControls = document.querySelector(".theme-controls");
+    themeControls.style.display = this.checked ? "flex" : "none";
+  });
 });
